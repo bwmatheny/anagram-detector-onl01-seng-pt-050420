0f810abfcc2922a61b7acdfe136a2842db63000f
Brian Matheny <bwmatheny@gmail.com>
2020-05-20 17:23:07 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,9 +1,12 @@
 class Anagram
-attr_accessor :word1 :list
+attr_accessor :word1
 
   def initialize(word)
-    @word1 = []
-    @word1 << word.split("")
+    @word1 = word
+  end
+
+  def match(list)
+    list.select {|word| word.split("").sort == @word1.split("").sort}
   end
 
 end
